6b5c4813e9c5c8dca31818d82186dc8a2a5f0886
özcan çetin <cetin445@gmail.com>
2022-06-27 14:35:08 +0300

    rest operator was used and props was sent

---
+++ src/components/main/Card.jsx
@@ -0,0 +1,24 @@
+
+
+//! normalde card componenti oluşturmadan da main içerisinde halledebilirdim. Ancak props çalışmak için card componenti oluşturdum.
+
+const Card = (data) => {
+    console.log(data); //todo datadan ne geldiğiini buradan görebilirsin.
+    const {id, title, image, desc} = data;
+    return(
+        <div className="cards">
+            <div className="title">
+            <h1>{title}</h1>
+            </div>
+
+            <img src={data.image} alt="" />
+
+            <div className="card-over">
+                <p>{desc}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default Card;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,35 @@
 import "./main.scss";
+import {data} from "../../helpers/data";
+import Card from "./Card";
 
 const Main = () => {
+  console.log(data)
   return (
-    <div>Main</div>
+    <div className="card-container">
+      {data.map((item)=> <Card {...item} /> )}
+    </div>
   )
 }
 
 export default Main
+
+//! *********************************
+// const Main = () => {
+//   console.log(data)
+//   return (
+//     <div className="card-container">
+//       {data.map((item)=>{
+//         const {id, title, image, desc} = item;
+//         return(
+//           <div className="cards">
+//             <h1>{title}</h1>
+//             <img src={image} alt="" />
+//             <p>{desc}</p>
+//           </div>
+//         )
+//       })}
+//       </div>
+//   )
+// }
+
+// export default Main
